e86dc4d793eb45ce5aa5120f66c1608ae0191a9b
Stephane Bellity <sbellity@gmail.com>
2018-05-24 18:32:01 +0200

    make sure properties is not null

--- server/events/page.js
+++ server/events/page.js
@@ -5,7 +5,7 @@ export default function handlePage(payload = {}, context = {}) {
   const { handle_pages } = ship.settings || {};
   if (handle_pages === false ) { return false; }
 
-  const { properties } = payload;
+  const { properties = {} } = payload;
   if (!properties.name && payload.name) {
     properties.name = payload.name;
   }
